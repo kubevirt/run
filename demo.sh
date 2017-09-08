302ad438de67056916550c614e9bb9edd8bfe3f1
Fabian Deutsch <fabiand@fedoraproject.org>
2017-09-08 11:59:10 +0200

    Update demo.sh script to reflect new minikube demo
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- demo.sh
+++ demo.sh
@@ -1,51 +1,24 @@
 #!/bin/bash
 
-. /etc/os-release
-
 die() { echo "FATAL: $@" >&2 ; exit 1 ; }
 title() { echo "== $@ ==" ; }
 info() { echo "INFO: $@" ; }
 capture() { eval $@ 2>&1 > .capture.log && rm .capture.log || { cat .capture.log ; rm .capture.log ; die "Failed to run: $@" ; } ; }
 
-install_dependencies() {
-  info "Checking dependencies"
-  if [[ "$ID" == "fedora" ]]; then
-    for PKG in qemu-system-x86 libguestfs-tools-c make git; do
-      if ! dnf -q list installed $PKG ; then
-        die "Please install '$PKG'"
-      fi
-    done
-  elif [[ "$ID" == "ubuntu" ]]; then
-    info "Please ensure to setup libguestfs correctly: http://libguestfs.org/guestfs-faq.1.html#downloading-installing-compiling-libguestfs"
-    info "Eventually you need to run this script using sudo"
-    for PKG in qemu-system-x86 libguestfs-tools make git; do
-      if ! dpkg -s $PKG >/dev/null ; then
-        die "Please install '$PKG'"
-      fi
-    done
-  else
-    die "Unsupported operating system '$ID'. We will be happy to merge patches to support your OS as well."
-  fi
-}
-
 clone_and_run() {
   info "Cloning repository ..."
-  if [[ -d "/var/tmp/kubevirt-demo" ]]; then
+  if [[ -d "/var/tmp/run-kubevirt-demo" ]]; then
     info "Reusing existing clone"
-    cd kubevirt-demo
+    cd run-kubevirt-demo
     capture git pull
   else
-    capture "git clone -q https://github.com/kubevirt/demo.git kubevirt-demo"
-    cd kubevirt-demo
+    capture "git clone -q https://github.com/kubevirt/demo.git run-kubevirt-demo"
+    cd run-kubevirt-demo
   fi
-  info "Building the demo (this can take a while) - Downloads a few hundred MB of data (tail -f $(realpath $PWD/.capture.log))"
-  capture make clean build
-  info "All done - Running the demo"
+  info "Running the demo"
   ./run-demo.sh
 }
 
 title "Preparing to run the KubeVirt Demo"
 cd /var/tmp/
-install_dependencies
 clone_and_run
-title "Demo has ended"
